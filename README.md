a6786eb4066fd79009de29ef96bb017485c78131
Francisco Mat <francisco@f5sites.com>
2018-06-14 19:00:11 -0300

    1st commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Briefing Maker PHP 2008
+by Francisco Mat
+
+The intent was to create service standards and improve quality and scalabity. Project stopped.
+
+![Briefing Maker PHP 2008](2018-06-14-18-briefing-maker-2008.png)
+
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Briefing Maker</title>
+<link href="briefingmaker.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="container">
+	<div id="header">
+	<h1 class="briefing">Briefing</h1><h1 class="maker">Maker</h1>
+	<h2 class="slogam">A padronização definitiva para briefings</h2>
+	</div>
+	
+	<div id="content">
+		<p>Com o Briefing Maker você pode criar seu Briefing de forma padronizada e ainda exportá-lo para PDF</p>
+		<p>Essa versão do sistema permite que se crie um Briefin para execução de serviços para a WEB, em breve mais</p>
+		<form>
+		<h4>Selecione o serviço desejado</h4>
+		<input type="checkbox" value="Criação de advergames" />Criação de advergames<br />
+		<input type="checkbox" value="Criação de site" />Criação de site<br />
+		<input type="checkbox" value="Instalação de loja virtual" />Instalação de loja virtual<br />
+		<input type="checkbox" value="Emissão de Boleto Bancário" />Emissão de Boleto Bancário<br />
+		<input type="checkbox" value="Outros serviços com flash ou php ou banco de dados" />Outros serviços com flash ou php ou banco de dados<br />
+		<br />
+		<div id="ser_site">
+		<table id="sites">
+			<tr class="linhaPagina">
+				<td>
+				<h4>Título da página</h4>
+				<input type="text" value="titulo" size="25"/>
+				</td>
+				<td>
+				<h4>Descrição do conteúdo</h4>
+				<input type="text" value="titulo" size="80"/>
+				</td>
+			</tr>
+		</table>
+		<a href="#">Adicionar Página</a> | <a href="#">Remover Página</a>
+		</div>
+		</form>
+		
+	</div>
+</div>
+</body>
+</html>
